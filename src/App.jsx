21d76b5da4f36959863a494f45eac28300b61827
wollllll <sakin50guramu@gmail.com>
2024-06-29 19:39:37 +0900

    fix select

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import {
     Tabs,
     TextField
 } from "@mui/material";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {TabPanel} from "./components/TabPanel.jsx";
 import axios from "axios";
 import {IssueLogTable} from "./components/IssueLogTable.jsx";
@@ -23,12 +23,32 @@ function App() {
 
     const [tab, setTab] = useState(Number(url.searchParams.get('tab')))
     const [type, setType] = useState(null)
-    const [mondayDate, setMondayDate] = useState('2024-06-17')
-    const [mondayDateFrom, setMondayDateFrom] = useState('2024-06-17')
-    const [mondayDateTo, setMondayDateTo] = useState('2024-06-17')
-    const [item, setItem] = useState('assign_count')
-    const [userName, setUserName] = useState('藤井 勝史')
     const [issueLogs, setIssueLogs] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    const [form, setForm] = useState({
+        date: '2024-06-17',
+        date_from: '2024-06-17',
+        date_to: '2024-06-17',
+        item: 'assign_count',
+        user_name: null,
+        users: [],
+    })
+
+    useEffect(() => {
+        (async () => {
+            const response = await axios.get(apiUrl, {
+                params: {
+                    route: 'users'
+                }
+            })
+            const users = response.data.users.map(user => user.name)
+            const [userName] = users
+
+            setForm({...form, users, user_name: userName})
+            setIsLoading(false)
+        })()
+    }, [form])
 
     const changeTab = (value) => {
         setTab(value)
@@ -45,17 +65,17 @@ function App() {
 
         switch (type) {
             case 1:
-                params.date = mondayDate
+                params.date = form.date
                 break
             case 2:
-                params.item = item
-                params.date_from = mondayDateFrom
-                params.date_to = mondayDateTo
+                params.item = form.item
+                params.date_from = form.date_from
+                params.date_to = form.date_to
                 break
             case 3:
-                params.user_name = userName
-                params.date_from = mondayDateFrom
-                params.date_to = mondayDateTo
+                params.user_name = form.user_name
+                params.date_from = form.date_from
+                params.date_to = form.date_to
                 break
         }
 
@@ -67,151 +87,151 @@ function App() {
 
     return (
         <>
-            <Container maxWidth="lg">
-                <Tabs value={tab} onChange={(event, value) => changeTab(value)}>
-                    <Tab label="日付指定" />
-                    <Tab label="項目フィルタ" />
-                    <Tab label="担当者フィルタ" />
-                </Tabs>
-                <TabPanel value={tab} index={0}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={4}>
-                            <FormControl fullWidth>
-                                <TextField
-                                    id="date"
-                                    label="日付"
-                                    type="date"
-                                    value={mondayDate}
-                                    onChange={(e) => setMondayDate(e.target.value)}
-                                />
-                            </FormControl>
+            {isLoading && (
+                <Container maxWidth="lg">
+                    loading...
+                </Container>
+            )}
+            {!isLoading && (
+                <Container maxWidth="lg">
+                    <Tabs value={tab} onChange={(event, value) => changeTab(value)}>
+                        <Tab label="日付指定"/>
+                        <Tab label="項目フィルタ"/>
+                        <Tab label="担当者フィルタ"/>
+                    </Tabs>
+                    <TabPanel value={tab} index={0}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={4}>
+                                <FormControl fullWidth>
+                                    <TextField
+                                        id="date"
+                                        label="日付"
+                                        type="date"
+                                        value={form.date}
+                                        onChange={(e) => setForm({...form, date: e.target.value})}
+                                    />
+                                </FormControl>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                    <Button variant="contained" sx={{mt: 2}} onClick={() => showResult(1)}>表示</Button>
-                </TabPanel>
-                <TabPanel value={tab} index={1}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={4}>
-                            <FormControl fullWidth>
-                                <InputLabel id="item">項目</InputLabel>
-                                <Select
-                                    labelId="item"
-                                    label="項目"
-                                    value={item}
-                                    onChange={(e) => setItem(e.target.value)}
-                                >
-                                    <MenuItem value="assign_count">担当チケット</MenuItem>
-                                    <MenuItem value="due_week_count">今週期限</MenuItem>
-                                    <MenuItem value="resolve_count">処理済み</MenuItem>
-                                    <MenuItem value="expire_count">期限切れ</MenuItem>
-                                    <MenuItem value="change_count">期限日変更</MenuItem>
-                                    <MenuItem value="add_count">追加</MenuItem>
-                                    <MenuItem value="close_count">消化</MenuItem>
-                                </Select>
-                            </FormControl>
+                        <Button variant="contained" sx={{mt: 2}} onClick={() => showResult(1)}>表示</Button>
+                    </TabPanel>
+                    <TabPanel value={tab} index={1}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={4}>
+                                <FormControl fullWidth>
+                                    <InputLabel id="item">項目</InputLabel>
+                                    <Select
+                                        labelId="item"
+                                        label="項目"
+                                        value={form.item}
+                                        onChange={(e) => setForm({...form, item: e.target.value})}
+                                    >
+                                        <MenuItem value="assign_count">担当チケット</MenuItem>
+                                        <MenuItem value="due_week_count">今週期限</MenuItem>
+                                        <MenuItem value="resolve_count">処理済み</MenuItem>
+                                        <MenuItem value="expire_count">期限切れ</MenuItem>
+                                        <MenuItem value="change_count">期限日変更</MenuItem>
+                                        <MenuItem value="add_count">追加</MenuItem>
+                                        <MenuItem value="close_count">消化</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <FormControl fullWidth>
+                                    <TextField
+                                        id="date_from"
+                                        label="日付"
+                                        type="date"
+                                        value={form.date_from}
+                                        onChange={(e) => setForm({...form, date_from: e.target.value})}
+                                    />
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <FormControl fullWidth>
+                                    <TextField
+                                        id="date_to"
+                                        label="日付"
+                                        type="date"
+                                        value={form.date_to}
+                                        onChange={(e) => setForm({...form, date_to: e.target.value})}
+                                    />
+                                </FormControl>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={4}>
-                            <FormControl fullWidth>
-                                <TextField
-                                    id="date_from"
-                                    label="日付"
-                                    type="date"
-                                    value={mondayDateFrom}
-                                    onChange={(e) => setMondayDateFrom(e.target.value)}
-                                />
-                            </FormControl>
+                        <Button variant="contained" sx={{mt: 2}} onClick={() => showResult(2)}>表示</Button>
+                    </TabPanel>
+                    <TabPanel value={tab} index={2}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={4}>
+                                <FormControl fullWidth>
+                                    <InputLabel id="user_name">担当者</InputLabel>
+                                    <Select
+                                        labelId="user_name"
+                                        label="担当者"
+                                        value={form.user_name}
+                                        onChange={(e) => setForm({...form, user_name: e.target.value})}
+                                    >
+                                        {
+                                            form.users.map((name, index) => {
+                                                return (
+                                                    <MenuItem key={index} value={name}>{name}</MenuItem>
+                                                )
+                                            })
+                                        }
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <FormControl fullWidth>
+                                    <TextField
+                                        id="date_from"
+                                        label="日付"
+                                        type="date"
+                                        value={form.date_from}
+                                        onChange={(e) => setForm({...form, date_from: e.target.value})}
+                                    />
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <FormControl fullWidth>
+                                    <TextField
+                                        id="date_to"
+                                        label="日付"
+                                        type="date"
+                                        value={form.date_to}
+                                        onChange={(e) => setForm({...form, date_to: e.target.value})}
+                                    />
+                                </FormControl>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={4}>
-                            <FormControl fullWidth>
-                                <TextField
-                                    id="date_to"
-                                    label="日付"
-                                    type="date"
-                                    value={mondayDateTo}
-                                    onChange={(e) => setMondayDateTo(e.target.value)}
-                                />
-                            </FormControl>
-                        </Grid>
-                    </Grid>
-                    <Button variant="contained" sx={{mt: 2}} onClick={() => showResult(2)}>表示</Button>
-                </TabPanel>
-                <TabPanel value={tab} index={2}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={4}>
-                            <FormControl fullWidth>
-                                <InputLabel id="user_name">担当者</InputLabel>
-                                <Select
-                                    labelId="user_name"
-                                    label="担当者"
-                                    value={userName}
-                                    onChange={(e) => setUserName(e.target.value)}
-                                >
-                                    <MenuItem value="藤井 勝史">藤井 勝史</MenuItem>
-                                    <MenuItem value="小平 晋悟">小平 晋悟</MenuItem>
-                                    <MenuItem value="荒井 哲也">荒井 哲也</MenuItem>
-                                    <MenuItem value="工藤 匠悟">工藤 匠悟</MenuItem>
-                                    <MenuItem value="宮前 慶一郎">宮前 慶一郎</MenuItem>
-                                    <MenuItem value="加藤 誉大">加藤 誉大</MenuItem>
-                                    <MenuItem value="津谷 宙隼">津谷 宙隼</MenuItem>
-                                    <MenuItem value="木村 流輝">木村 流輝</MenuItem>
-                                    <MenuItem value="杉山 虎太朗">杉山 虎太朗</MenuItem>
-                                    <MenuItem value="牟田園 明">牟田園 明</MenuItem>
-                                    <MenuItem value="イン・ヲンジエ">イン・ヲンジエ</MenuItem>
-                                    <MenuItem value="川上 駿季">川上 駿季</MenuItem>
-                                    <MenuItem value="赤松 晏奈">赤松 晏奈</MenuItem>
-                                    <MenuItem value="園田 大地">園田 大地</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <FormControl fullWidth>
-                                <TextField
-                                    id="date_from"
-                                    label="日付"
-                                    type="date"
-                                    value={mondayDateFrom}
-                                    onChange={(e) => setMondayDateFrom(e.target.value)}
-                                />
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <FormControl fullWidth>
-                                <TextField
-                                    id="date_to"
-                                    label="日付"
-                                    type="date"
-                                    value={mondayDateTo}
-                                    onChange={(e) => setMondayDateTo(e.target.value)}
-                                />
-                            </FormControl>
-                        </Grid>
-                    </Grid>
-                    <Button variant="contained" sx={{mt: 2}} onClick={() => showResult(3)}>表示</Button>
-                </TabPanel>
+                        <Button variant="contained" sx={{mt: 2}} onClick={() => showResult(3)}>表示</Button>
+                    </TabPanel>
 
-                {type === 1 && (
-                    <IssueLogTable issueLogs={issueLogs} />
-                )}
+                    {type === 1 && (
+                        <IssueLogTable issueLogs={issueLogs}/>
+                    )}
 
-                {type === 2 && (
-                    <LineChart
-                        sx={{mt: 2}}
-                        height={600}
-                        series={issueLogs.result}
-                        xAxis={[{ scaleType: 'point', data: issueLogs.dates}]}
-                    />
-                )}
+                    {type === 2 && (
+                        <LineChart
+                            sx={{mt: 2}}
+                            height={600}
+                            series={issueLogs.result}
+                            xAxis={[{scaleType: 'point', data: issueLogs.dates}]}
+                        />
+                    )}
 
-                {type === 3 && (
-                    <LineChart
-                        sx={{mt: 2}}
-                        height={600}
-                        series={issueLogs.result}
-                        xAxis={[{ scaleType: 'point', data: issueLogs.dates}]}
-                    />
-                )}
-            </Container>
-    </>
+                    {type === 3 && (
+                        <LineChart
+                            sx={{mt: 2}}
+                            height={600}
+                            series={issueLogs.result}
+                            xAxis={[{scaleType: 'point', data: issueLogs.dates}]}
+                        />
+                    )}
+                </Container>
+            )}
+        </>
     )
 }
 
